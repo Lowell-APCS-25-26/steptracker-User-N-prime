6ccfd2fc0aff866110de8739fd5c525c16d41785
User-N-prime <nilam@s.sfusd.edu>
2026-04-21 04:23:35 +0000

    built functioning addDailySteps + activeDays + averageSteps functions

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,29 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int totalSteps;
+    private int totalDays;
+    private int totalDaysActive;
+    private int minActive;
+
+    public StepTracker(int activityThreshold) {
+        minActive = activityThreshold;
+        totalSteps = totalDays = totalDaysActive = 0;
+    }
+    public void addDailySteps(int stepCount) {
+        totalSteps += stepCount;
+        totalDays++;
+        if (stepCount >= minActive) {
+            totalDaysActive++;
+        }
+    }
+    public int activeDays() {
+        return totalDaysActive;
+    }
+    public double averageSteps() {
+        if (totalDays == 0) {
+            return 0.0;
+        }
+        return (double) totalSteps / totalDays;
+    }
 }
